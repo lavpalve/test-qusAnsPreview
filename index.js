2dc9c0b15464eb76484d9b2d3638de2635dd7f71
lavpalve <53850659+lavpalve@users.noreply.github.com>
2021-11-14 20:48:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+function previewAns(iconBoxId, ansBoxNumId){
+    let iconEle = document.getElementById(iconBoxId);
+    let ansBoxEle = document.getElementById(ansBoxNumId);
+
+    iconEle.style.display = "none";
+    ansBoxEle.style.display = "block";
+
+}
